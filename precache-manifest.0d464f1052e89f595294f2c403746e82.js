f8fbded2de0ca43aa56dc1f27f5628cc2f18b5c8
ZeyadALbazlamit <61747480+ZeyadALbazlamit@users.noreply.github.com>
2020-07-24 22:26:10 +0400

    Updates

---
+++ precache-manifest.0d464f1052e89f595294f2c403746e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5535446bff9fc68fa4c8c9774bde2c44",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "5c6049422999f5e433dd",
+    "url": "/React/static/css/main.7d1eb789.chunk.css"
+  },
+  {
+    "revision": "379233a5f5cbb62f7288",
+    "url": "/React/static/js/2.d92a8b35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React/static/js/2.d92a8b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6049422999f5e433dd",
+    "url": "/React/static/js/main.8a0c179a.chunk.js"
+  },
+  {
+    "revision": "b92de6a649160246144d",
+    "url": "/React/static/js/runtime-main.9909f5cf.js"
+  }
+]);
